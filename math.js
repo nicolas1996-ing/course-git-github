5c1f90c8b250d175aedcf0f449de3582d6617d50
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2025-02-21 13:05:22 -0500

    feat: add console.table in math.js

--- math.js
+++ math.js
@@ -1,1 +1,7 @@
 // math operators
+console.table([
+  { operator: "+", description: "addition" },
+  { operator: "-", description: "subtraction" },
+  { operator: "*", description: "multiplication" },
+  { operator: "/", description: "division" },
+]);
