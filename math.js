0604e6ee02bce13d87b1d51edbf97dec4a9cef5f
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2025-03-11 17:21:13 -0500

    update sum description in math.js

--- math.js
+++ math.js
@@ -1,6 +1,6 @@
 // math operators
 console.table([
-  { operator: "+", description: "sum-conflict" },
+  { operator: "+", description: "sum" },
   { operator: "-", description: "subtraction" },
   { operator: "*", description: "multiplication" },
   { operator: "/", description: "division" },
