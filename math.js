41fdb4a9840dd1159e171fceeaa7224e45f93b99
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2025-03-11 17:07:48 -0500

    add aditional changes in math.js

--- math.js
+++ math.js
@@ -1,6 +1,6 @@
 // math operators
 console.table([
-  { operator: "+", description: "addition" },
+  { operator: "+", description: "addition-(+)" },
   { operator: "-", description: "subtraction" },
   { operator: "*", description: "multiplication" },
   { operator: "/", description: "division" },
