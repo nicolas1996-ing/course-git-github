357621b4af4ba53287d92cfc6309dbb4673d0f82
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2025-03-13 19:02:54 -0500

    init npm project

--- math.js
+++ math.js
@@ -10,3 +10,17 @@ console.log("sum-conflict", 1 + 1);
 console.log("subtraction", 1 - 1);
 console.log("multiplication", 1 * 1);
 console.log("division", 1 / 1);
+
+// crear un programa que pida al usario una entrada por consola e imprima el resultado
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// solicitar al usuario dos numeros e imprimir la suma
+readline.question("Enter a number: ", (number1) => {
+  readline.question("Enter another number: ", (number2) => {
+    console.log("The sum is: ", parseInt(number1) + parseInt(number2));
+    readline.close();
+  });
+});
